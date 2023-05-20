0dae0f759450a2e3d76e1b8b87657a235145fd1f
Aidaar <aidar21mamatov@gmail.com>
2023-05-20 11:16:46 +0600

    add text on click

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const btn = document.getElementById('btn')
 
+const text = document.createElement('p')
+
 
 addEventListener('click', ()=> {
     btn.style.background = 'blue'
+    btn.before(text)
+    text.innerHTML = `
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Minima consectetur odit perspiciatis beatae officia recusandae 
+    corrupti expedita accusamus fugiat, ipsam facere vero, 
+    perferendis, earum eaque pariatur optio dolor excepturi quas!
+    `
 })
